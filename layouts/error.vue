d87ebeab504e93af9f77c3e3fc47db632ebdcb86
schnee25 <schnee.2114@gmail.com>
2021-05-06 03:20:33 +0900

    エラーページの作成

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Error! 404</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
+
+// nuxtコンポーネント
+<nust />
+入れちゃダメ
